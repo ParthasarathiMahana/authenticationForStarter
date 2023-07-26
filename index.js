a91fe0fafca86fde8d9e9c61043a32a55b0b7b96
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-07-26 11:36:12 +0530

    MVC architecture implemented for home page.

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static("assets"));
 app.use(express.urlencoded());
 
-app.get('/',(req, res)=>{
-    res.render("home");
-})
+// app.get('/',(req, res)=>{
+//     res.render("home");
+// })
+app.use('/', require('./routes'));
 
 app.listen(PORT,(err)=>{
     if(err){
