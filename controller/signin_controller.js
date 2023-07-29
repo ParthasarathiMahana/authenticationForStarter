16d2ba285daccf61bce3ace8deddbca8ab7e9c21
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-07-29 12:51:31 +0530

    reset password functionality is added.

--- controller/signin_controller.js
+++ controller/signin_controller.js
@@ -20,8 +20,9 @@ module.exports.resetPassword = async(req, res)=>{
             return console.log("user not found for password reset");
         }
         if(req.body.new_password === req.body.confirm_password){
-            user.updateOne({password: bcrypt.hash(req.body.new_password, 10)});
-            return res.render('homeAfterLogin')
+            let user1 = await User.findByIdAndUpdate(user._id,{password: await bcrypt.hash(req.body.new_password, 10)})
+            console.log("password changed");
+            return res.render('homeAfterLogin',{user_email:user.email})
         }
         console.log("password do not match");
         return res.render('home');
